7ed0c2d27e3a11ea0cd6d704c670ab1ece34d998
Nikandr Marhal <nikandr@adjico.com>
2022-06-14 10:09:34 +0300

    Implemented cell with code layout

--- CanYouReallySearchGitHub/Supporting Files/SceneDelegate.swift
+++ CanYouReallySearchGitHub/Supporting Files/SceneDelegate.swift
@@ -10,8 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = UINavigationController(rootViewController: RepositoryListViewController())
+        window.rootViewController = rootViewController
+        window.windowScene = windowScene
+        self.window = window
+
+        window.makeKeyAndVisible()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
--- CanYouReallySearchGitHub/View/RepositoryCell.swift
+++ CanYouReallySearchGitHub/View/RepositoryCell.swift
@@ -7,26 +7,181 @@
 
 import UIKit
 
-class RepositoryCell: UITableViewCell {
-    // MARK: - Outlets
+final class RepositoryCell: UITableViewCell {
+    // MARK: - UI
 
-    @IBOutlet var userAvatarImage: UIImageView!
-    @IBOutlet var userNameLabel: UILabel!
-    @IBOutlet var repositoryNameLabel: UILabel!
-    @IBOutlet var descriptionLabel: UILabel!
-    @IBOutlet var stargazersCountLabel: UILabel!
-    @IBOutlet var languageLabel: UILabel!
-    @IBOutlet var seenImage: UIImageView!
+    private lazy var topContentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userAvatarImage)
+        view.addSubview(userNameLabel)
 
-    // MARK: - Data
+        view.heightAnchor.constraint(equalToConstant: 40).isActive = true
 
-    weak var viewModel: RepositoryCellViewModelType? {
-        willSet(viewModel) {
-            guard let viewModel = viewModel else { return }
-            fillCell(withDataOf: viewModel)
-            bindObservers(to: viewModel)
-        }
+        userAvatarImage.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        userAvatarImage.heightAnchor.constraint(equalTo: userAvatarImage.widthAnchor).isActive = true
+        userAvatarImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        userAvatarImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        userNameLabel.leadingAnchor.constraint(equalTo: userAvatarImage.trailingAnchor, constant: 5).isActive = true
+        userNameLabel.trailingAnchor.constraint(greaterThanOrEqualTo: view.trailingAnchor, constant: 0).isActive = true
+        userNameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        return view
+    }()
+
+    private lazy var userAvatarImage: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var userNameLabel: UILabel = {
+        let view = UILabel()
+        view.font = .preferredFont(forTextStyle: .subheadline)
+        view.textColor = .systemGray
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var repositoryNameLabel: UILabel = {
+        let view = UILabel()
+        view.font = .preferredFont(forTextStyle: .headline)
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.topAnchor.constraint(equalTo: topContentView.bottomAnchor, constant: 8).isActive = true
+
+        return view
+    }()
+
+    private lazy var descriptionLabel: UILabel = {
+        let view = UILabel()
+        view.font = .preferredFont(forTextStyle: .body)
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.heightAnchor.constraint(equalToConstant: 43).isActive = true
+        view.topAnchor.constraint(equalTo: repositoryNameLabel.bottomAnchor, constant: 5).isActive = true
+
+        return view
+    }()
+
+    private lazy var bottomContentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(starIcon)
+        view.addSubview(stargazersCountLabel)
+        view.addSubview(languageIcon)
+        view.addSubview(languageLabel)
+
+        view.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8).isActive = true
+
+        starIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        starIcon.heightAnchor.constraint(equalTo: starIcon.widthAnchor).isActive = true
+        starIcon.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        starIcon.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        stargazersCountLabel.leadingAnchor.constraint(equalTo: starIcon.trailingAnchor, constant: 5).isActive = true
+        stargazersCountLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        languageIcon.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        languageIcon.heightAnchor.constraint(equalTo: languageIcon.widthAnchor).isActive = true
+        languageIcon.leadingAnchor.constraint(equalTo: stargazersCountLabel.trailingAnchor, constant: 30).isActive = true
+        languageIcon.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        languageLabel.leadingAnchor.constraint(equalTo: languageIcon.trailingAnchor, constant: 5).isActive = true
+        languageLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        seenImage.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        seenImage.heightAnchor.constraint(equalTo: seenImage.widthAnchor).isActive = true
+        seenImage.leadingAnchor.constraint(greaterThanOrEqualTo: languageLabel.trailingAnchor, constant: 0).isActive = true
+        seenImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        seenImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        return view
+    }()
+
+    private lazy var starIcon: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "star")?.withTintColor(.darkGray)
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var stargazersCountLabel: UILabel = {
+        let view = UILabel()
+        view.font = .preferredFont(forTextStyle: .subheadline)
+        view.textColor = .darkGray
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var languageIcon: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "chevron.left.forwardslash.chevron.right")?.withTintColor(.darkGray)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private lazy var languageLabel: UILabel = {
+        let view = UILabel()
+        view.font = .preferredFont(forTextStyle: .subheadline)
+        view.textColor = .darkGray
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var seenImage: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "eye")?.withTintColor(.darkGray)
+        return view
+    }()
+
+    // MARK: - Initialization
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupLayout()
     }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        contentView.addSubview(topContentView)
+        contentView.addSubview(repositoryNameLabel)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(bottomContentView)
+
+        topContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        topContentView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        topContentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+
+        repositoryNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        repositoryNameLabel.trailingAnchor.constraint(greaterThanOrEqualTo: contentView.trailingAnchor, constant: 0).isActive = true
+
+        descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        descriptionLabel.trailingAnchor.constraint(greaterThanOrEqualTo: contentView.trailingAnchor, constant: 0).isActive = true
+
+        bottomContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bottomContentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bottomContentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+    }
+
+    // MARK: - Methods
 
     func fillCell(withDataOf viewModel: RepositoryCellViewModelType) {
         userAvatarImage.setContentDownloaded(from: viewModel.avatarURL)
